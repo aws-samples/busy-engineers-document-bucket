525fdd323b6517ac5bd70cc28851bcc63848b8d5
lizroth <30636882+lizroth@users.noreply.github.com>
2019-11-02 18:21:43 +0000

    Use prettier on bin/*.ts too.

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
-import { CMKStack } from '../lib/kms-cmk-stack';
-import { DocumentBucketStack } from '../lib/document-bucket-stack';
-import { WebsiteStack } from '../lib/website-stack';
+import "source-map-support/register";
+import cdk = require("@aws-cdk/core");
+import { CMKStack } from "../lib/kms-cmk-stack";
+import { DocumentBucketStack } from "../lib/document-bucket-stack";
+import { WebsiteStack } from "../lib/website-stack";
 
 const REGION_A = "us-east-2";
 const REGION_B = "us-west-2";
@@ -13,17 +13,21 @@ const app = new cdk.App();
 
 // Initialize CMK Stacks
 new CMKStack(app, "BusyEngineersFaytheCMKStack", {
-    env: {region: REGION_A},
-    alias: "FaytheCMK" });
+  env: { region: REGION_A },
+  alias: "FaytheCMK"
+});
 
 new CMKStack(app, "BusyEngineersWalterCMKStack", {
-    env: {region: REGION_B},
-    alias: "WalterCMK" });
+  env: { region: REGION_B },
+  alias: "WalterCMK"
+});
 
 // Initialize Document Bucket resources
 new DocumentBucketStack(app, "BusyEngineersDocumentBucketStack", {
-    env: {region: WEBSITE_REGION}});
+  env: { region: WEBSITE_REGION }
+});
 
 // Initialize client website resources
 new WebsiteStack(app, "BusyEngineersWebsiteStack", {
-    env: {region: WEBSITE_REGION}});
+  env: { region: WEBSITE_REGION }
+});
