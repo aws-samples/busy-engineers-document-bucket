c8eee18321966cde1ee9f324aadd802b8a8b981a
Ryan Emery <ryanemer@amazon.com>
2019-11-18 11:12:10 -0800

    feat: export cfn output to state
    Add a simple script to export the CloudFormation outputs to a state file.
    The outputs from `config.toml` can be looked up as needed.

---
+++ cdk/bin/cdk_state.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import aws = require("aws-sdk");
+import fs = require("fs");
+import toml = require("@iarna/toml");
+
+const cf = new aws.CloudFormation()
+
+!(async () => {
+  const { Exports = [] } = await cf.listExports().promise()
+
+  const cfnState  = Exports
+    .filter(validExport)
+    .reduce((memo, { Name, Value }) => {
+      memo[Name] = Value
+      return memo
+    }, {} as { [key: string]: string })
+  
+  fs.writeFileSync("../exercises/busy_engineers_state.toml", toml.stringify({ state: cfnState }), {encoding: 'utf8'})
+})()
+
+function validExport(value: aws.CloudFormation.Export) : value is Required<aws.CloudFormation.Export> {
+  return !!value.Name && !!value.Value
+}
