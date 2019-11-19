f1cd13790bde77cd15b9c69713427102e6fd5be4
Ryan Emery <ryanemer@amazon.com>
2019-11-19 09:50:13 -0800

    feat: Add KMS information

--- cdk/bin/cdk_state.ts
+++ cdk/bin/cdk_state.ts
@@ -3,28 +3,20 @@
 // Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import aws = require("aws-sdk");
 import fs = require("fs");
 import toml = require("@iarna/toml");
 import { config } from "../lib/config"
+import { cfnExports } from "../lib/exports"
 import untildify = require('untildify')
 
-const cf = new aws.CloudFormation()
 const state_file = untildify(config.base.state_file)
 
 !(async () => {
-  const { Exports = [] } = await cf.listExports().promise()
+  const cfnState  = await cfnExports()
 
-  const cfnState  = Exports
-    .filter(validExport)
-    .reduce((memo, { Name, Value }) => {
-      memo[Name] = Value
-      return memo
-    }, {} as { [key: string]: string })
-  
-  fs.writeFileSync(state_file, toml.stringify({ state: cfnState }), {encoding: 'utf8', flag: 'w'})
+  fs.writeFileSync(
+    state_file,
+    toml.stringify({ state: cfnState }),
+    {encoding: 'utf8', flag: 'w'}
+  )
 })()
-
-function validExport(value: aws.CloudFormation.Export) : value is Required<aws.CloudFormation.Export> {
-  return !!value.Name && !!value.Value
-}
---
+++ cdk/bin/create_grants.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { cfnExports } from "../lib/exports"
+import { config } from "../lib/config"
+import { createGrant, getCurrentPrinciple } from "../lib/grant_helpers"
+import assert = require("assert")
+
+!(async () => {
+  const [cfnState, currentPrinciple]  = await Promise.all([
+    cfnExports(),
+    getCurrentPrinciple()
+  ])
+
+  const faytheKeyId = cfnState[config.faythe.export]
+  const walterKeyId = cfnState[config.walter.export]
+
+  assert(faytheKeyId && walterKeyId, 'KeyIds do not exist. Deploy stacks')
+  
+  await Promise.all([
+    createGrant(faytheKeyId, currentPrinciple),
+    createGrant(walterKeyId, currentPrinciple),
+  ])
+
+})()
