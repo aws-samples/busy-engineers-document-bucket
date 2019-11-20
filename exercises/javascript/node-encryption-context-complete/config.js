cfa64665f6ba934f4397432427788eb3c17ed59c
Ryan Emery <ryanemer@amazon.com>
2019-11-20 11:59:16 -0800

    fix: prettier formating

--- exercises/javascript/node-encryption-context-complete/config.js
+++ exercises/javascript/node-encryption-context-complete/config.js
@@ -1,31 +1,29 @@
-const toml = require("@iarna/toml")
-const fs = require("fs")
-const untildify = require("untildify")
+const toml = require("@iarna/toml");
+const fs = require("fs");
+const untildify = require("untildify");
 
-const config = toml.parse(
-  fs.readFileSync("../../config.toml", "utf8")
-)
+const config = toml.parse(fs.readFileSync("../../config.toml", "utf8"));
 
 const { state } = toml.parse(
   fs.readFileSync(untildify(config.base.state_file), "utf8")
-)
+);
 
-config.state =  {
+config.state = {
   bucketName() {
-    return state[config.document_bucket.bucket.export]
+    return state[config.document_bucket.bucket.export];
   },
 
   tableName() {
-    return state[config.document_bucket.document_table.export]
+    return state[config.document_bucket.document_table.export];
   },
 
-  getFaytheCMK(){
-    return state[config.faythe.export]
+  getFaytheCMK() {
+    return state[config.faythe.export];
   },
 
-  getWalterCMK(){
-    return state[config.walter.export]
+  getWalterCMK() {
+    return state[config.walter.export];
   }
-}
+};
 
-module.exports = config
+module.exports = config;
--- exercises/javascript/node-encryption-context-complete/list.js
+++ exercises/javascript/node-encryption-context-complete/list.js
@@ -1,27 +1,26 @@
-const { DynamoDB } = require('aws-sdk')
-const ddb = new DynamoDB.DocumentClient()
-const config = require('./config')
-const { sort_key, object_target } = config.document_bucket.document_table
-const TableName = config.state.tableName()
+const { DynamoDB } = require("aws-sdk");
+const ddb = new DynamoDB.DocumentClient();
+const config = require("./config");
+const { sort_key, object_target } = config.document_bucket.document_table;
+const TableName = config.state.tableName();
 
-module.exports = list
+module.exports = list;
 
 async function list({ ExclusiveStartKey, Limit } = {}) {
-
   return ddb
     .scan({
       TableName,
-      FilterExpression: '#RangeKey = :rkey',
+      FilterExpression: "#RangeKey = :rkey",
       ExpressionAttributeValues: {
-        ':rkey': object_target
+        ":rkey": object_target
       },
       ExpressionAttributeNames: {
-        '#RangeKey': sort_key
+        "#RangeKey": sort_key
       },
       ExclusiveStartKey,
       Limit
     })
-    .promise()
+    .promise();
 }
 
 /* TODO Explorations
