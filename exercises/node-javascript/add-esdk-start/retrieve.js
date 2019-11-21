05868f7942338bb18aa723972a040efe7d7e7831
seebees <ryanemer@amazon.com>
2019-11-21 10:30:02 -0800

    feat(javascript): Add add-esdk (#48)
    Add the start and complete code for adding the AWS Encryption SDK.

---
+++ exercises/node-javascript/add-esdk-start/retrieve.js
@@ -0,0 +1,17 @@
+/* ADD-ESDK-START:
+ * Add the @aws-crypto/client-node dependency.
+ * You will need the decrypt stream as well as the KMS keyring.
+ * Look at the config file to see how to pull the Faythe CMK.
+ * Then pipe the encrypted stream to the decrypt stream: Profit.
+ */
+
+const { S3 } = require("aws-sdk");
+const s3 = new S3();
+const config = require("./config");
+const Bucket = config.state.bucketName();
+
+module.exports = retrieve;
+
+function retrieve(Key, { expectedContext, expectedContextKeys } = {}) {
+  return s3.getObject({ Bucket, Key }).createReadStream();
+}
