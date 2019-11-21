7165f0f7491b47eb344e5033544f5f84ad5439d6
lizroth <30636882+lizroth@users.noreply.github.com>
2019-11-21 17:43:53 +0000

    fix(python): Add pointer comments to complete exercise to help students.

--- exercises/python/encryption-context-complete/src/document_bucket/api.py
+++ exercises/python/encryption-context-complete/src/document_bucket/api.py
@@ -73,6 +73,7 @@ class DocumentBucketOperations:
         plaintext, header = aws_encryption_sdk.decrypt(
             source=encrypted_data, key_provider=self.master_key_provider
         )
+        # ENCRYPTION-CONTEXT-COMPLETE
         if not expected_context_keys <= header.encryption_context.keys():
             error_msg = (
                 "Encryption context assertion failed! "
@@ -87,11 +88,13 @@ class DocumentBucketOperations:
                 f"but got {header.encryption_context}!"
             )
             raise AssertionError(error_msg)
+        # ENCRYPTION-CONTEXT-COMPLETE
         return DocumentBundle.from_data_and_context(
             plaintext, header.encryption_context
         )
 
     def store(self, data: bytes, context: Dict[str, str] = {}) -> PointerItem:
+        # ENCRYPTION-CONTEXT-COMPLETE
         encrypted_data, header = aws_encryption_sdk.encrypt(
             source=data,
             key_provider=self.master_key_provider,
