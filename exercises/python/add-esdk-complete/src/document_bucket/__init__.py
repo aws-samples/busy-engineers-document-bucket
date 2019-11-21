7b50a487d053b30bbf408514408e2f4af23fcc45
lizroth <30636882+lizroth@users.noreply.github.com>
2019-11-21 17:51:52 +0000

    Create Python add-esdk exercise base.

---
+++ exercises/python/add-esdk-complete/src/document_bucket/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+import aws_encryption_sdk  # type: ignore
+import boto3  # type: ignore
+import toml
+
+from .api import DocumentBucketOperations
+from .config import config
+
+
+def initialize() -> DocumentBucketOperations:
+    # Load the pointers to CloudFormation resources that you just deployed
+    state = toml.load(os.path.expanduser(config["base"]["state_file"]))["state"]
+
+    # Now you have the identifiers of the CloudFormation resources loaded,
+    # Set up your S3 Bucket for the Document Bucket
+    bucket = boto3.resource("s3").Bucket(state["DocumentBucket"])
+    # Set up your DynamoDB Table for the Document Bucket
+    table = boto3.resource("dynamodb").Table(state["DocumentTable"])
+    # MULTI-CMK-START
+    # Pull configuration of KMS resources
+    faythe_cmk = state["FaytheCMK"]
+    # And the Master Key Provider configuring how to use KMS
+    # MULTI-CMK-START
+    cmk = [faythe_cmk]
+    mkp = aws_encryption_sdk.KMSMasterKeyProvider(key_ids=[cmk])
+
+    # Set up the API to interact with the Document Bucket using all these resources
+    operations = DocumentBucketOperations(bucket, table, mkp)
+
+    return operations
