3dda87ccf622a8e8cf1ecd52b2808c5b2b3c0d0f
lizroth <30636882+lizroth@users.noreply.github.com>
2019-11-24 19:35:31 +0000

    fix(java): Forgot the initialization helper for Java exercises.

--- exercises/java/encryption-context-complete/src/main/java/sfw/example/esdkworkshop/App.java
+++ exercises/java/encryption-context-complete/src/main/java/sfw/example/esdkworkshop/App.java
@@ -3,8 +3,43 @@
 
 package sfw.example.esdkworkshop;
 
+import com.amazonaws.encryptionsdk.kms.KmsMasterKeyProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
 public class App {
+
+  // The names of resources from the configuration file must exactly match those
+  // keys for the automatic mapping.
+  // CHECKSTYLE:OFF AbbreviationAsWordInName
+  public static Api initializeDocumentBucket() {
+    // Load the TOML State file with the information about launched CloudFormation resources
+    State state = new State(Config.contents.base.state_file);
+
+    // Configure DynamoDB client
+    String tableName = state.contents.DocumentTable;
+    AmazonDynamoDB ddbClient = AmazonDynamoDBClientBuilder.defaultClient();
+
+    // Configure S3 client
+    String bucketName = state.contents.DocumentBucket;
+    AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+
+    // Load configuration of KMS resources
+    String faytheCMK = state.contents.FaytheCMK;
+    String walterCMK = state.contents.WalterCMK;
+
+    // Set up the Master Key Provider to use KMS
+    KmsMasterKeyProvider mkp =
+        KmsMasterKeyProvider.builder().withKeysForEncryption(faytheCMK, walterCMK).build();
+
+    // Construct the API
+    return new Api(ddbClient, tableName, s3Client, bucketName, mkp);
+  }
+  // CHECKSTYLE:ON AbbreviationAsWordInName
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    // Interact with the Document Bucket here or in jshell (mvn jshell:run)
   }
 }
