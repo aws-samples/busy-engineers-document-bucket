9bb8ce8218e8a1d37162979232fab53ffa88baf0
lizroth <30636882+lizroth@users.noreply.github.com>
2019-12-01 20:07:05 -0800

    fix(java): Add missing short license headers.

--- exercises/java/multi-cmk-start/src/main/java/sfw/example/esdkworkshop/DocumentBucketException.java
+++ exercises/java/multi-cmk-start/src/main/java/sfw/example/esdkworkshop/DocumentBucketException.java
@@ -1,3 +1,6 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package sfw.example.esdkworkshop;
 
 /** Exception to wrap errors encountered during Document Bucket operations. */
